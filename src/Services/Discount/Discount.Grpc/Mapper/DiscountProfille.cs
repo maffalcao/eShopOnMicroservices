e2712ddd509ff796241443d1049bece0675920f2
Marco Falcão <maffalcao@gmail.com>
2023-09-30 16:02:19 -0300

    add grpc service GetDiscount method

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfille.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Mapper;
+
+public class DiscountProfille: Profile
+{
+    public DiscountProfille()
+    {
+        CreateMap<Coupon, CouponModel>();        
+    }
+}
