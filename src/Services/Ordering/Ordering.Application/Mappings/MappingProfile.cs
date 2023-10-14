33d68dd8ecdbd34af16e7f06fe5b97b2585144df
Marco Falcão <maffalcao@gmail.com>
2023-10-14 16:15:08 -0300

    developing order.application layer

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+
+    }
+}
