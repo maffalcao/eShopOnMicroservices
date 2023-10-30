137ddbe32451f88dc33b06180fde615a55ecb030
Marco Falcão <maffalcao@gmail.com>
2023-10-30 18:47:28 -0300

    infrastructure dependency injections

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,16 +1,25 @@
+using Microsoft.Extensions.Configuration;
 using Ordering.Application.DI;
+using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
 builder.Services.AddApplicationServices();
-//builder.Services.AddInfraestructureServices();
+builder.Services.AddInfrastructureServices(configuration);
 
 var app = builder.Build();
 
