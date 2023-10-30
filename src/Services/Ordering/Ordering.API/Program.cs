9e5c1c1f5455dc25de899e9cbe806c0930e467bd
Marco Falcão <maffalcao@gmail.com>
2023-10-30 19:26:54 -0300

    run ordering migrations at the startup

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.Configuration;
+using Ordering.API.Extensions;
 using Ordering.Application.DI;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +24,14 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(configuration);
 
 var app = builder.Build();
+
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed
+        .SeedAsync(context, logger)
+        .Wait();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
